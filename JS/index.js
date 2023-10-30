0e64c59fcb9d01625e07a37a1641e6d446f3c366
rrwrq <shuxin_huang@fitnyc.edu>
2023-10-30 22:32:14 +0800

    update

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#workButton").hover(function(){
+        $(this).css("background-color", "violet"
+            );
+
+        },function(){
+            $(this).css("background-color", "gray");
+        
+        });
+
+
+
+});
