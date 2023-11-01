664e603680e43cbf54255109b13595097145f064
rrwrq <shuxin_huang@fitnyc.edu>
2023-11-01 14:18:35 +0800

    update

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-$("#workButton").hover(function(){
+/*$("#workButton").hover(function(){
     $(this).css("background-color", "violet"
         );
 
@@ -7,4 +7,40 @@ $("#workButton").hover(function(){
         
     });
 
+    const btn = document.getElementById('btn');
 
+    btn.addEventListener('click', function onClick(event) {
+      // 👇️ change background color
+      document.body.style.backgroundColor = 'salmon';
+    
+      // 👇️ optionally change text color
+      // document.body.style.color = 'white';
+    });
+    */
+
+/*
+$( ".navigation" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+});
+*/
+
+
+
+$( "#go" ).hover (function() {
+    $( ".block" ).first().animate({
+        bottom: 10
+    }, {
+        duration: 1000,
+        top:10
+      }
+    );
+  });
+
+
+/*
+$(".navigation").hover(function() {
+  $(this).animate({height: "100px"}, 500, 'easeOutBounce');
+});
+
+*/
